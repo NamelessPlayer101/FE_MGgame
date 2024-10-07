d42a184ec5cf04c47c2e76bbac878cd8c04ba0b6
hailt <luonghai5622@gmail.com>
2024-10-07 14:33:34 +0700

    Temp

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -4,13 +4,9 @@ import style from "./Chat.module.scss";
 function Chat() {
   return (
     <div className={style.container}>
-      <div className={style.content}>
-        <ul id="messages"></ul>
-        <form id="form" action="">
-          <input id="input" autoComplete="off" />
-          <button>Send</button>
-        </form>
-      </div>
+      <div className={style.header}>Chat</div>
+      <div className={style.content}></div>
+      <div className={style.footer}>Bottom</div>
     </div>
   );
 }
