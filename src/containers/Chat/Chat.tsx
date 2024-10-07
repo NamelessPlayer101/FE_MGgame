31d50cb1596a1ce8654b9ddda0ed45df467e5840
hailt <luonghai5622@gmail.com>
2024-10-07 15:53:09 +0700

    Temp

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -2,14 +2,35 @@ import React from "react";
 import style from "./Chat.module.scss";
 
 function Chat() {
+  const [message, setMessage] = React.useState("");
+
+  const onSubmit = () => {
+    console.log(message);
+  };
+
   return (
     <div className={style.container}>
       <div className={style.header}>Chat</div>
       <div className={style.content}></div>
-      <div className={style.footer}>Bottom</div>
+      <div className={style.footer}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && message) {
+              setMessage("");
+              onSubmit();
+            }
+          }}
+          className={style.input}
+          type="text"
+        />
+        <div onClick={message ? onSubmit : () => {}} className={style.button}>
+          Send
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chat;
-
