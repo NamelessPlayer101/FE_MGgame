4c68d3f8a4f4730c98f4c6332f7c3f77ff69cddd
hailt <luonghai5622@gmail.com>
2024-10-07 17:02:19 +0700

    Temp

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -1,17 +1,57 @@
 import React from "react";
 import style from "./Chat.module.scss";
+import { socket } from "../../services/socket";
 
 function Chat() {
   const [message, setMessage] = React.useState("");
+  const [messages, setMessages] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    socket.on("connection", (socket) => {});
+
+    socket.on("chat", (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    return () => {
+      socket.off("connection");
+      socket.off("chat");
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const message = document.querySelector(`.${style.message}`);
+    const content = document.querySelector(`.${style.content}`);
+    if (message && content) {
+      const isAtBottom =
+        content.scrollHeight - content.scrollTop === content.clientHeight;
+      if (messages.length > 0 && !isAtBottom) {
+        content.scrollTop = content.scrollHeight;
+      }
+    }
+  }, [messages]);
 
   const onSubmit = () => {
-    console.log(message);
+    socket.emit("chat", message);
   };
 
   return (
     <div className={style.container}>
-      <div className={style.header}>Chat</div>
-      <div className={style.content}></div>
+      <div
+        onClick={() => {
+          console.log(JSON.stringify(messages));
+        }}
+        className={style.header}
+      >
+        Chat
+      </div>
+      <div className={style.content}>
+        {messages.map((message, index) => (
+          <div key={index} className={style.message}>
+            {message}
+          </div>
+        ))}
+      </div>
       <div className={style.footer}>
         <input
           value={message}
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,4 +1,7 @@
-require('dotenv').config();
+// require('dotenv').config({ path: __dirname+'/.env' });
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.SOCKET_URL);
+console.log(__dirname);
+
+// export const socket = io(process.env.SOCKET_URL);
+export const socket = io('http://localhost:4444');
